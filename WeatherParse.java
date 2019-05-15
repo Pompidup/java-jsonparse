878c8c16882a23518d94b4d9ba237d5f2c07eb03
Pompidup <julienauvray28@gmail.com>
2019-05-15 13:12:24 +0200

    Change on class WeatherParse for parsing and print data in JSON file

--- WeatherParse.java
+++ WeatherParse.java
@@ -26,14 +26,41 @@ public class WeatherParse {
         
         // TODO parser le fichier
 
+        JSONParser parser = new JSONParser();
+        Object jsonParsed = null;
+        try {
+            jsonParsed = parser.parse(jsonFile);
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+
         // TODO récupérer la racine du document
+
+        JSONObject root = (JSONObject) jsonParsed;
 
         // TODO afficher la valeur de l'attribut "name" de la racine
 
+        String name = (String) root.get("name");
+        System.out.println("City name: "+name);
+
         // TODO afficher les valeurs des attributs "lat" et "lon" de l'élément "coord" contenu dans la racine
+
+        JSONObject coord = (JSONObject) root.get("coord");
+        Double lon = (Double) coord.get("lon");
+        Double lat = (Double) coord.get("lat");
+        System.out.println("City latitude: "+lat);
+        System.out.println("City longitude: "+lon);
 
         // TODO parcourir tous les éléments de "weather" et afficher le contenu de "main"
 
+        JSONArray weather = (JSONArray) root.get("weather");
+
+        for (int i = 0; i < weather.size(); i++) {
+            JSONObject weatherObj = (JSONObject) weather.get(i);
+            String main = (String) weatherObj.get("main");
+            System.out.println("Weather: "+main);
+
+        }
         /*
             Résultat attendu :
             * City name: London
